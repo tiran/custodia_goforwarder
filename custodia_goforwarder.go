c65c17382ff6d7f598969ae9149401948894f7cd
Christian Heimes <cheimes@redhat.com>
2016-07-15 18:16:30 +0200

    Parse cgroup file and make TLS config more flexible
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- custodia_goforwarder.go
+++ custodia_goforwarder.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
@@ -13,16 +14,22 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 	"syscall"
 )
 
 var (
 	upstream   = flag.String("upstream", "https://localhost:14443/secrets", "upstream server")
-	certFile   = flag.String("cert", "client.cert", "A PEM eoncoded certificate file.")
-	keyFile    = flag.String("key", "client.key", "A PEM encoded private key file.")
-	caFile     = flag.String("cacert", "ca.pem", "A PEM eoncoded CA's certificate file.")
+	certFile   = flag.String("cert", "", "A PEM eoncoded certificate file.")
+	keyFile    = flag.String("key", "", "A PEM encoded private key file.")
+	caFile     = flag.String("cacert", "", "A PEM eoncoded CA's certificate file.")
 	socketFile = flag.String("socketfile", "./forwarder.sock", "Unix Socket File")
+)
+
+var (
+	dockerRegexp *regexp.Regexp
+	rktRegexp    *regexp.Regexp
 )
 
 type Forwarder struct {
@@ -30,34 +37,50 @@ type Forwarder struct {
 	Transport *http.Transport
 }
 
-func NewForwarder(certFile string, keyFile string, caFile string, upstream string) (*Forwarder, error) {
+func NewForwarder(upstream string) (*Forwarder, error) {
+	log.Printf("Forwarding requests to %s", upstream)
 	url, err := url.Parse(upstream)
 	if err != nil {
 		return nil, err
 	}
+	transport := &http.Transport{}
+	return &Forwarder{Upstream: url, Transport: transport}, nil
+}
 
-	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return nil, err
-	}
-
-	// Load CA cert
+func (forwarder *Forwarder) SetCACert(caFile string) error {
+	log.Printf("Loading CA file %s", caFile)
 	caCert, err := ioutil.ReadFile(caFile)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
+	if forwarder.Transport.TLSClientConfig == nil {
+		forwarder.Transport.TLSClientConfig = &tls.Config{}
+	}
 
-	// Setup HTTPS client
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
+	tlsConfig := forwarder.Transport.TLSClientConfig
+	if tlsConfig.RootCAs == nil {
+		tlsConfig.RootCAs = x509.NewCertPool()
 	}
+	ok := tlsConfig.RootCAs.AppendCertsFromPEM(caCert)
+	if !ok {
+		return errors.New("Failed to load CA cert")
+	}
+	return nil
+}
+
+func (forwarder *Forwarder) SetClientCert(certFile, keyFile string) error {
+	log.Printf("Loading client cert %s : %s", certFile, keyFile)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return err
+	}
+	if forwarder.Transport.TLSClientConfig == nil {
+		forwarder.Transport.TLSClientConfig = &tls.Config{}
+	}
+	tlsConfig := forwarder.Transport.TLSClientConfig
+	tlsConfig.Certificates = []tls.Certificate{cert}
 	tlsConfig.BuildNameToCertificate()
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
-
-	return &Forwarder{Upstream: url, Transport: transport}, nil
+	return nil
 }
 
 func singleJoiningSlash(a, b string) string {
@@ -110,9 +133,15 @@ func (forwarder *Forwarder) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	ctyp, cid, err := getContainerId(int(ucred.Pid))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	peercred := fmt.Sprintf("%d:%d:%d", ucred.Pid, ucred.Uid, ucred.Gid)
 	outreq.Header.Set("CUSTODIA_PEER_CRED", peercred)
 	outreq.Header.Set("CUSTODIA_PEER_LABEL", label)
+	outreq.Header.Set("CUSTODIA_CONTAINER_TYPE", ctyp)
+	outreq.Header.Set("CUSTODIA_CONTAINER_ID", cid)
 	// outreq.Header.Set("REMOTE_USER", peercred)
 
 	res, err := forwarder.Transport.RoundTrip(outreq)
@@ -132,6 +161,8 @@ func (forwarder *Forwarder) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 		outresp.Header.Set("Content-Type", "text/plain; charset=utf-8")
 		outresp.Header.Set("CUSTODIA_PEER_CRED", peercred)
 		outresp.Header.Set("CUSTODIA_PEER_LABEL", label)
+		outresp.Header.Set("CUSTODIA_CONTAINER_TYPE", ctyp)
+		outresp.Header.Set("CUSTODIA_CONTAINER_ID", cid)
 		outresp.Write(bufrw)
 		bufrw.Flush()
 		log.Println(outresp)
@@ -142,6 +173,8 @@ func (forwarder *Forwarder) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 	*outresp = *res
 	outresp.Header.Set("CUSTODIA_PEER_CRED", peercred)
 	outresp.Header.Set("CUSTODIA_PEER_LABEL", label)
+	outresp.Header.Set("CUSTODIA_CONTAINER_TYPE", ctyp)
+	outresp.Header.Set("CUSTODIA_CONTAINER_ID", cid)
 	outresp.Write(bufrw)
 	bufrw.Flush()
 }
@@ -171,13 +204,52 @@ func getPeerInfo(conn net.Conn) (ucred *syscall.Ucred, label string, err error) 
 	return
 }
 
+func getContainerId(pid int) (typ string, id string, err error) {
+	file, err := os.Open(fmt.Sprintf("/proc/%d/cgroup", pid))
+	if err != nil {
+		return "", "", err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var line string
+		var rs []string
+		line = scanner.Text()
+		rs = dockerRegexp.FindStringSubmatch(line)
+		if len(rs) == 2 {
+			return "docker", rs[1], nil
+		}
+		rs = rktRegexp.FindStringSubmatch(line)
+		if len(rs) == 2 {
+			id = strings.Replace(rs[1], "\x2d", "-", -1)
+			return "rkt", id, nil
+		}
+	}
+	return "", "", nil
+}
+
 func main() {
 	flag.Parse()
 
-	forwarder, err := NewForwarder(*certFile, *keyFile, *caFile, *upstream)
+	forwarder, err := NewForwarder(*upstream)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if *caFile != "" {
+		err = forwarder.SetCACert(*caFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if *certFile != "" {
+		err = forwarder.SetClientCert(*certFile, *keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	dockerRegexp = regexp.MustCompile(`/docker-([0-9a-f]{64})\.scope`)
+	rktRegexp = regexp.MustCompile(`/machine-rkt\\x2([\\x0-9a-f]+)\.scope`)
 
 	if !strings.Contains(*socketFile, "/") {
 		log.Fatal(errors.New("Server address is not a path"))
@@ -190,6 +262,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Listening on %s", *socketFile)
 	err = http.Serve(listener, forwarder)
 	log.Fatal(err)
 }
